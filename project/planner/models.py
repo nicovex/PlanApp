dc4c469e2c65b30d142b05db0904e5d46bd4362e
Nicolas Vexelman <nicovexel@gmail.com>
2024-05-21 19:45:28 -0300

    creado app planner y core

---
+++ project/planner/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Estado(models.Model):
+    estado = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self) -> str:
+        return self.estado
+
+class Tarea(models.Model):
+    nombre = models.CharField(max_length=200)
+    # responsable = models.OneToOneField()
+    estado_id = models.ForeignKey(Estado, null=True, blank=True, on_delete=models.SET_NULL)
+
+
+class Proyecto(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+    descripcion = models.CharField(max_length=250, null=True, blank=True, verbose_name="descripción")
+    fecha_inicio = models.DateField(null=True, blank=True, default=timezone.now)
+    fecha_fin = models.DateField(null=True, blank=True, default=timezone.now)
+    tareas = models.ManyToManyField(Tarea)
+    
+    def __str__(self) -> str:
+        """Representa una instancia del modelo como una cadena de texto"""
+        return self.nombre
